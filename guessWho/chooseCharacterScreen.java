cab5804bb6591c8f2c5f9145308ecf35bc5318f0
khoale88 <lenguyenkhoa1988@gmail.com>
2016-10-07 21:26:50 -0700

    add DisplayBox class as a super class for CharacterBox, responsible for display objects into world

--- guessWho/chooseCharacterScreen.java
+++ guessWho/chooseCharacterScreen.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.List;
+import java.util.Set;
 import java.util.ArrayList;
 /**
  * Write a description of class chooseCharacterScreen here.
@@ -35,67 +36,7 @@ public class chooseCharacterScreen extends World implements ButtonRunnable
      * That is: create the initial objects and add them to the world.
      */
     private void prepare()
-    {
-        /*chooseCharTitle choosechartitle = new chooseCharTitle();
-        addObject(choosechartitle,876,641);
-        choosechartitle.setLocation(894,640);
-
-        // CharacterBox charBox = new CharacterBox(925,460);
-        // //addObject(charBox,getWidth()/2,getHeight()/2);
-        // addObject(charBox,getWidth()/2,600);
-        // charBox.addCharacter(new Char1());
-        // charBox.addCharacter(new Char2());
-        // charBox.addCharacter(new Char3());
-        // charBox.addCharacter(new Char4());
-        // charBox.addCharacter(new Char5());
-        // charBox.addCharacter(new Char6());
-        // charBox.addCharacter(new Char7());
-        // charBox.addCharacter(new Char8());
-        // charBox.addCharacter(new Char9());
-
-        Char1 char1 = new Char1();
-        Char2 char2 = new Char2();
-        Char3 char3 = new Char3();
-        Char4 char4 = new Char4();
-        Char5 char5 = new Char5();
-        Char6 char6 = new Char6();
-        Char7 char7 = new Char7();
-        Char8 char8 = new Char8();
-        Char9 char9 = new Char9();
-
-        addObject(char1,342,308);
-        addObject(char2,510,594);        
-        addObject(char3,953,309);        
-        addObject(char4,810,595);       
-        addObject(char5,1095,594);       
-        addObject(char6,1354,593);       
-        addObject(char7,658,308);        
-        addObject(char8,215,595);
-        addObject(char9,1225,302);
-
-        char1.setLocation(342,308);
-        char2.setLocation(510,594);
-        char3.setLocation(953,309);
-        char4.setLocation(810,595);
-        char5.setLocation(1095,594);
-        char6.setLocation(1354,593);
-        char7.setLocation(658,308);
-        char8.setLocation(215,595);
-        char9.setLocation(1225,302);
-        
-        allChars = getObjects(Character.class);
-        
-        buttonDisabled buttondisabled = new buttonDisabled();
-        addObject(buttondisabled,743,774);*/
-        //buttondisabled.setLocation(743,774);
-        
-        
-        //testing
-        
-        
-        //my new implementation
-        
-        
+    {         
         GreenfootImage bg = this.getBackground();
         chooseCharBox.setImage(bg);
         addObject(chooseCharBox,getWidth()/2,getHeight()/2);
@@ -107,8 +48,7 @@ public class chooseCharacterScreen extends World implements ButtonRunnable
         addObject(new chooseCharTitle(),876,641);
 
         chooseCharBox.addAllChars(gameSession.getAllFromFullSet());
-        
-        
+
         //setup button confirm to check condition pass out from chooseCharBox
         buttonConfirm.addConditionalObj(chooseCharBox); 
         addObject(buttonConfirm,743,774);
