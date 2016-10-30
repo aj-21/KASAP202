211d4e1655c3cd2eb8a17f972da434f806f09dd3
AJ Bell <anthonyjbell21@gmail.com>
2016-10-30 16:45:48 -0700

    Update Char Attributes

--- guessWho/Char2.java
+++ guessWho/Char2.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Char2 here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @SPAAK
+ * 
  */
 public class Char2 extends Character
 {   
