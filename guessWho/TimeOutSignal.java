b33b5aded160d72950e7feb6a3d7f1aee8336261
khoale88 <lenguyenkhoa1988@gmail.com>
2016-11-08 17:06:36 -0800

    print time

--- guessWho/TimeOutSignal.java
+++ guessWho/TimeOutSignal.java
@@ -10,11 +10,15 @@ public class TimeOutSignal
 {
     Timer timer;
     GuessWhoState state;
+    long seconds;
+    int print = 0;
     public TimeOutSignal(GuessWhoState state, long seconds)
     {
         timer = new Timer();
         timer.schedule(new Signal(),seconds*1000);
+        timer.schedule(new Print(),0,1000);
         this.state = state;
+        this.seconds = seconds;
     }
     
     class Signal extends TimerTask{
@@ -25,4 +29,13 @@ public class TimeOutSignal
         }
     }
     
+    class Print extends TimerTask{
+        public void run()
+        {
+            if (print == 0)
+                print = (int)seconds;
+            System.out.println(print);
+            print --;
+        }
+    }
 }
